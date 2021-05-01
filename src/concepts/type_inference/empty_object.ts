e1afcccd2c1955f9afd57cdc6ddf2a2d997f2de9
Piechowicz, Krzysztof <krzysztof.piechowicz@adesso.de>
2021-05-01 15:15:53 +0200

    object literal type + JS native object

---
+++ src/concepts/type_inference/empty_object.ts
@@ -0,0 +1,10 @@
+
+// inferred type: {}
+const obj = {};
+
+// error, because inferred type doesn't contain "msg" field
+// obj.msg = "Hello";
+
+// but JS native object is a dictionary (string -> value), so it's possible to assign any string key to the object
+// (!) works if strict = false
+obj["msg"] = "Hello";
