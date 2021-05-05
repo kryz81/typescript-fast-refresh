37c69c443a95d8f58cfe261fbe222fa8fc92e61f
kryz <kpiechowicz@gmail.com>
2021-05-05 18:40:28 +0200

    Update empty_object.ts

--- src/concepts/type_inference/empty_object.ts
+++ src/concepts/type_inference/empty_object.ts
@@ -7,4 +7,4 @@ const obj = {};
 
 // but JS native object is a dictionary (string -> value), so it's possible to assign any string key to the object
 // (!) works if strict = false
-obj["msg"] = "Hello";
+//obj["msg"] = "Hello";
